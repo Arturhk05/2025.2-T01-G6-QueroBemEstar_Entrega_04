c734d312ef0948d9d37d02cbb2aadf5ca25c275d
Arturhk05 <arturhandow@gmail.com>
2025-11-20 17:58:47 -0300

    backend(feat): implement recipe sanitization helpers and update GetAllRecipesController

--- backend/src/modules/receitas/presentation/controllers/GetAllRecipesController.ts
+++ backend/src/modules/receitas/presentation/controllers/GetAllRecipesController.ts
@@ -1,14 +1,20 @@
 import { IController } from "@/shared/protocols/controller"
-import { Recipe } from "../../domain/entities/recipe"
 import { HttpResponse } from "@/shared/protocols/http"
 import { IGetAllRecipes } from "../../application/ports/IGetAllRecipes"
 import { ok } from "@/shared/helpers/HttpHelpers"
+import {
+  SanitizationHelpers,
+  SanitizedRecipe,
+} from "@/shared/helpers/SanitizationHelpers"
 
-export class GetAllRecipesController implements IController<null, Recipe[]> {
+export class GetAllRecipesController
+  implements IController<null, SanitizedRecipe[]>
+{
   constructor(private readonly getAllRecipes: IGetAllRecipes) {}
 
-  async handle(): Promise<HttpResponse<Recipe[] | Error>> {
+  async handle(): Promise<HttpResponse<SanitizedRecipe[] | Error>> {
     const recipes = await this.getAllRecipes.execute()
-    return ok(recipes)
+    const sanitizedRecipes = SanitizationHelpers.sanitizeRecipes(recipes)
+    return ok(sanitizedRecipes)
   }
 }
